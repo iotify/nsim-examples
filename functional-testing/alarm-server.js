db2a4687ad3b994df7207867459b8ce5d58743f3
admin <hello@iotify.io>
2020-05-27 14:48:13 +0200

    Create alarm-server.js
    Initial commit

---
+++ functional-testing/alarm-server.js
@@ -0,0 +1,33 @@
+// Important: Change the ##myuniquetopic## below to a unique string to prevent 
+// MQTT topic collision over public server. 
+
+// This a small server code which listens to a fire sensor and triggers alarm when
+// temperature exceeds a certain threshold. 
+
+var mqtt = require('mqtt')
+var client  = mqtt.connect('mqtt://broker.hivemq.com:1883')
+
+client.on('connect', function () {
+  client.subscribe('/[##myuniquetopic##]/temperature/0', function (err) {
+    if (err) {
+      console.error('Error subscribing to the topic');
+    }
+    else{
+        console.log('Subscribed to topic.')
+    }
+  })
+})
+
+client.on('message', function (topic, message) {
+
+  let cmd = JSON.parse(message.toString());
+  // Check if parsed payload is valid and contains temperature
+  if (cmd && cmd.temperature)
+  {
+     client.publish('/iotify/command/0', JSON.stringify({
+        alarm: (cmd.temperature > 80) ? true: false
+     }))
+  }
+})
+
+console.log("Running Simple MQTT test server")
